e475e1a81b8d73e0f7124695edc54614f4ff6488
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2024-08-03 15:27:17 -0300

    Validar Edição e exclusão de produto

--- app.js
+++ app.js
@@ -64,30 +64,53 @@ app.get("/", function (req, res) {
   });
 });
 
+//MAIN ROUTE with SITUATION
+app.get("/:situacao", function (req, res) {
+  //SQL
+  let sql = "SELECT * FROM produtos";
+
+  //Executar comando SQL
+  conexao.query(sql, function (err, result) {
+    res.render("forms", { produtos: result, situacao:req.params.situacao }); //Render handlebars file
+  });
+});
+
 //Cadaster Route
 app.post("/cadastrar", function (req, res) {
   /* console.log(req);
   console.log(req.files.imagem.name);
   req.files.imagem.mv(__dirname+'/imagens/'+req.files.imagem.name)   res.end();*/
 
-  let nome = req.body.nome;
-  let valor = req.body.valor;
-  let imagem = req.files.imagem.name;
+   try{
 
-  //SQL
-  let sql = `INSERT INTO produtos (nome, valor, imagem) VALUES ('${nome}', ${valor}, '${imagem}')`;
+    let nome = req.body.nome;
+    let valor = req.body.valor;
+    let imagem = req.files.imagem.name;
+    
+    //Validate product name and value
+    if(nome == '' || valor == '' || isNaN(valor)){
+      res.redirect('/falhaCadastro')
+    }else{
+        //SQL
+      let sql = `INSERT INTO produtos (nome, valor, imagem) VALUES ('${nome}', ${valor}, '${imagem}')`;
+    
+      //Executation command SQL
+    
+      conexao.query(sql, function (err, res) {
+        if (err) throw err;
+    
+        req.files.imagem.mv(__dirname + "/imagens/" + req.files.imagem.name); //mv is to send a file for a directory
+        console.log(res);
+      });
+    
+      //Return to the main route
+      res.redirect("/sucessoCadastro");
+    }
+   
 
-  //Executation command SQL
-
-  conexao.query(sql, function (err, res) {
-    if (err) throw err;
-
-    req.files.imagem.mv(__dirname + "/imagens/" + req.files.imagem.name);
-    console.log(res);
-  });
-
-  //Return to the main route
-  res.redirect("/");
+   }catch(err){
+    res.redirect('/falhaCadastro')
+   }
 });
 
 //Route to remove products
@@ -95,23 +118,29 @@ app.get("/remover/:codigo&:imagem", function (req, res) {
   // console.log(req.params.codigo);
   // console.log(req.params.imagem);
 
-  //SQL
-  let sql = `DELETE FROM produtos WHERE codigo = ${req.params.codigo}`;
+  //Exception handling
+  try{
+          //SQL
+      let sql = `DELETE FROM produtos WHERE codigo = ${req.params.codigo}`;
 
-  //Execute the SQL command
-  conexao.query(sql, function (err, retorno) {
-    if (err) throw err;
+      //Execute the SQL command
+      conexao.query(sql, function (err, retorno) {
+        if (err) throw err;
 
-    fs.unlink(__dirname + "/imagens/" + req.params.imagem, (err_img) => {
-      if (err_img) console.log("Falha ao remover imagem");
-      else {
-        console.log("Produto excluído");
-      }
-    });
-  });
+        fs.unlink(__dirname + "/imagens/" + req.params.imagem, (err_img) => {
+          if (err_img) console.log("Falha ao remover imagem");
+          else {
+            console.log("Produto excluído");
+          }
+        });
+      });
 
-  //Redirection
-  res.redirect("/");
+      //Redirection
+      res.redirect("/sucessoRemover");
+  }catch(err){
+      res.redirect('/falhaRemover')
+  }
+  
 });
 
 //Route to redirect for the forms of changes/edition
@@ -126,5 +155,63 @@ app.get("/formularioEditar/:codigo", function (req, res) {
     res.render('formsEditar', {produto:retorno[0]}) //SELECT command return a LIST (array), so like if want only one product, to acess item primary, uses [0]  
   })
 });
+
+
+
+
+//Route to edit products
+app.post('/editar', function(req, res){
+
+    let nome = req.body.nome;
+    let valor = req.body.valor;
+    let codigo = req.body.codigo;
+    let nomeImagem = req.body.nomeImagem;
+    //let imagem = req.files.imagem.name;
+
+
+  if(nome == '' || valor == '' || isNaN(valor)){
+    res.redirect('/falhaEdicao')
+  }else{
+
+
+  //Define edition type
+
+  try{
+    //Image object
+    let imagem = req.files.imagem;
+
+    //SQL
+    let sql = `UPDATE produtos SET nome='${nome}', valor='${valor}', imagem=${imagem.name} WHERE codigo=${codigo}`
+   
+    //Execute SQL command
+
+    conexao.query(sql, function(err, retorno){
+        //Case fall command SQL
+        if(err) throw err;
+
+        //Remove old imagen 
+        fs.unlink(__dirname + '/imagens/'+ nomeImagem, (erro_imagem)=>{
+          console.log('Falha ao remover a imagem.');
+
+        });
+
+       //Cadaster new imagen
+       imagem.mv(__dirname + '/imagens/' + imagem.name);
+    })
+  }catch(err){
+
+    //SQL
+    let sql = `UPDATE produtos SET nome='${nome}', valor=${valor} WHERE codigo=${codigo}`
+
+     //Execute SQL command
+     conexao.query(sql, function(err, retorno){
+      if(err) throw err;
+    });
+  }
+
+    //Redirect for main route
+    res.redirect('/sucessoCadastro')
+  }
+})
 //Server
 app.listen(8080);
